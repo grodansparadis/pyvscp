108e5206aa0b9ebac182dd06fb230a087f193d4a
Ake Hedman, Grodans Paradis AB <akhe@grodansparadis.com>
2018-11-28 08:12:33 +0100

    Syncing VSCP errors and events

--- vscp/vscp_class.py
+++ vscp/vscp_class.py
@@ -1,19 +1,3 @@
-# ******************************************************************************
-# VSCP (Very Simple Control Protocol)
-# http://www.vscp.org
-#
-# The MIT License (MIT)
-#
-# Copyright (c) 2000-2018 Ake Hedman, 
-# Grodans Paradis AB <info@grodansparadis.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
@@ -30,73 +14,72 @@
 #
 # ******************************************************************************
 
+
+#
 #            !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
-# This file may be a copy of the original file. This is because the file is
-# copied to other projects as a convinience. Thus editing the copy will not make
-# it to the original and will be overwritten.
-# The original file can be foud in the vscp software source tree under 
-# src/vscp/common
-
-VSCP_CLASS1_PROTOCOL                =            0
-VSCP_CLASS1_ALARM                   =            1
-VSCP_CLASS1_SECURITY                =            2
-VSCP_CLASS1_MEASUREMENT             =            10
-VSCP_CLASS1_DATA                    =            15
-VSCP_CLASS1_INFORMATION             =            20
-VSCP_CLASS1_CONTROL                 =            30
-VSCP_CLASS1_MULTIMEDIA              =            40
-VSCP_CLASS1_AOL                     =            50
-VSCP_CLASS1_MEASUREMENT64           =            60
-VSCP_CLASS1_MEASUREZONE             =            65
-VSCP_CLASS1_MEASUREMENT32           =            70
-VSCP_CLASS1_SETVALUEZONE            =            85
-VSCP_CLASS1_WEATHER                 =            90
-VSCP_CLASS1_WEATHER_FORECAST        =            95
-VSCP_CLASS1_PHONE                   =            100
-VSCP_CLASS1_DISPLAY                 =            102
-VSCP_CLASS1_REMOTE                  =            110
-VSCP_CLASS1_GPS                     =            206
-VSCP_CLASS1_WIRELESS                =            212
-VSCP_CLASS1_DIAGNOSTIC              =            506
-VSCP_CLASS1_ERROR                   =            508
-VSCP_CLASS1_LOG                     =            509
-VSCP_CLASS1_LAB                     =            510
-VSCP_CLASS1_LOCAL                   =            511
-
-# Level I classes on Level II
-
-VSCP_CLASS2_LEVEL1_PROTOCOL         =            ( 512 + 0 )
-VSCP_CLASS2_LEVEL1_ALARM            =            ( 512 + 1 )
-VSCP_CLASS2_LEVEL1_SECURITY         =            ( 512 + 2 )
-VSCP_CLASS2_LEVEL1_MEASUREMENT      =            ( 512 + 10 )
-VSCP_CLASS2_LEVEL1_DATA             =            ( 512 + 15 )
-VSCP_CLASS2_LEVEL1_INFORMATION      =            ( 512 + 20 )
-VSCP_CLASS2_LEVEL1_CONTROL          =            ( 512 + 30 )
-VSCP_CLASS2_LEVEL1_MULTIMEDIA       =            ( 512 + 40 )
-VSCP_CLASS2_LEVEL1_AOL              =            ( 512 + 50 )
-VSCP_CLASS2_LEVEL1_MEASUREMENT64    =            ( 512 + 60 )
-VSCP_CLASS2_LEVEL1_MEASUREZONE      =            ( 512 + 65 )
-VSCP_CLASS2_LEVEL1_MEASUREMENT32    =            ( 512 + 70 )
-VSCP_CLASS2_LEVEL1_SETVALUEZONE     =            ( 512 + 85 )
-VSCP_CLASS2_LEVEL1_WEATHER          =            ( 512 + 90 )
-VSCP_CLASS2_LEVEL1_WEATHER_FORECAST =            ( 512 + 95 )
-VSCP_CLASS2_LEVEL1_PHONE            =            ( 512 + 100 )
-VSCP_CLASS2_LEVEL1_REMOTE           =            ( 512 + 110 )
-VSCP_CLASS2_LEVEL1_GPS              =            ( 512 + 206 )
-VSCP_CLASS2_LEVEL1_WIRELESS         =            ( 512 + 212 )
-VSCP_CLASS2_LEVEL1_DIAGNOSTIC       =            ( 512 + 507 )
-VSCP_CLASS2_LEVEL1_ERROR            =            ( 512 + 508 )
-VSCP_CLASS2_LEVEL1_LOG              =            ( 512 + 509 )
-VSCP_CLASS2_LEVEL1_LAB              =            ( 512 + 510 )
-VSCP_CLASS2_LEVEL1_LOCAL            =            ( 512 + 511 )
-
-# Level II classes
-VSCP_CLASS2_PROTOCOL                =            ( 1024 )
-VSCP_CLASS2_CONTROL                 =            ( 1025 )
-VSCP_CLASS2_INFORMATION             =            ( 1026 )
-VSCP_CLASS2_TEXT2SPEECH             =            ( 1028 )
-VSCP_CLASS2_CUSTOM                  =            ( 1029 )
-VSCP_CLASS2_DISPLAY                 =            ( 1030 )
-VSCP_CLASS2_MEASUREMENT_STR         =            ( 1040 )
-VSCP_CLASS2_MEASUREMENT_FLOAT       =            ( 1060 )
-VSCP_CLASS2_VSCPD                   =            ( 65535 )
+#                           This file is auto-generated
+#                see https://github.com/grodansparadis/vscp-classes
+#                        Generated: 2018-11-27 23:36:02.091276
+#
+ 
+CLASS1_PROTOCOL                          = 0      # VSCP Protocol Functionality
+CLASS1_ALARM                             = 1      # Alarm functionality
+CLASS1_SECURITY                          = 2      # Security
+CLASS1_MEASUREMENT                       = 10     # Measurement
+CLASS1_DATA                              = 15     # Data
+CLASS1_INFORMATION                       = 20     # Information
+CLASS1_CONTROL                           = 30     # Control
+CLASS1_MULTIMEDIA                        = 40     # Multimedia
+CLASS1_AOL                               = 50     # Alert On LAN
+CLASS1_MEASUREMENT64                     = 60     # Double precision floating point measurement
+CLASS1_MEASUREZONE                       = 65     # Measurement with zone
+CLASS1_MEASUREMENT32                     = 70     # Single precision floating point measurement
+CLASS1_SETVALUEZONE                      = 85     # Set value with zone
+CLASS1_WEATHER                           = 90     # Weather
+CLASS1_WEATHER_FORECAST                  = 95     # Weather forecast
+CLASS1_PHONE                             = 100    # Phone
+CLASS1_DISPLAY                           = 102    # Display
+CLASS1_IR                                = 110    # IR Remote I/f
+CLASS1_GNSS                              = 206    # Position (GNSS)
+CLASS1_WIRELESS                          = 212    # Wireless
+CLASS1_DIAGNOSTIC                        = 506    # Diagnostic
+CLASS1_ERROR                             = 508    # Error
+CLASS1_LOG                               = 509    # Logging
+CLASS1_LABORATORY                        = 510    # Laboratory use
+CLASS1_LOCAL                             = 511    # Local use
+CLASS2_LEVEL1_PROTOCOL                   = 512    # Class2 Level I Protocol
+CLASS2_LEVEL1_ALARM                      = 513    # Class2 Level I Alarm
+CLASS2_LEVEL1_SECURITY                   = 514    # Class2 Level I Security
+CLASS2_LEVEL1_MEASUREMENT                = 522    # Class2 Level I Measurements
+CLASS2_LEVEL1_DATA                       = 527    # Class2 Level I Data
+CLASS2_LEVEL1_INFORMATION1               = 532    # Class2 Level I Information
+CLASS2_LEVEL1_CONTROL                    = 542    # Class2 Level I Control
+CLASS2_LEVEL1_MULTIMEDIA                 = 552    # Class2 Level I Multimedia
+CLASS2_LEVEL1_AOL                        = 562    # Class2 Level I AOL
+CLASS2_LEVEL1_MEASUREMENT64              = 572    # Class2 Level I Measurement64
+CLASS2_LEVEL1_MEASUREZONE                = 577    # Class2 Level I Measurementzone
+CLASS2_LEVEL1_MEASUREMENT32              = 582    # Class2 Level I Measuremet32
+CLASS2_LEVEL1_SETVALUEZONE               = 597    # Class2 Level I SetValueZone
+CLASS2_LEVEL1_WEATHER                    = 602    # Class2 Level I Weather
+CLASS2_LEVEL1_WEATHERFORECAST            = 607    # Class2 Level I Weather Forecast
+CLASS2_LEVEL1_PHONE                      = 612    # Class2 Level I Phone
+CLASS2_LEVEL1_DISPLAY                    = 614    # Class2 Level I Display
+CLASS2_LEVEL1_IR                         = 622    # Class2 Level I IR
+CLASS2_LEVEL1_GNSS                       = 718    # Class2 Level I GNSS
+CLASS2_LEVEL1_WIRELESS                   = 724    # Class2 Level I Wireless
+CLASS2_LEVEL1_DIAGNOSTIC                 = 1018   # Class2 Level I Diagnostic
+CLASS2_LEVEL1_ERROR                      = 1020   # Class2 Level I Error
+CLASS2_LEVEL1_LOG                        = 1021   # Class2 Level I Log
+CLASS2_LEVEL1_LABORATORY                 = 1022   # Class2 Level I Laboratory
+CLASS2_LEVEL1_LOCAL                      = 1023   # Class2 Level I Local
+CLASS2_PROTOCOL                          = 1024   # Level II Protocol Functionality
+CLASS2_CONTROL                           = 1025   # Level II Control
+CLASS2_INFORMATION                       = 1026   # Level II Information
+CLASS2_TEXT2SPEECH                       = 1027   # Text to speech
+CLASS2_CUSTOM                            = 1029   # Level II Custom
+CLASS2_DISPLAY                           = 1030   # Level II Display
+CLASS2_MEASUREMENT_STR                   = 1040   # Measurement string
+CLASS2_MEASUREMENT_FLOAT                 = 1060   # Measurement float
+CLASS2_VSCPD                             = 65535  # VSCP Daemon internal events
+ 
+ 
