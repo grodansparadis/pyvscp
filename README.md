307362666a5165e22e1f8a66ec89643a0fff035a
Ake Hedman, Grodans Paradis AB <akhe@grodansparadis.com>
2019-08-28 21:15:22 +0200

    Markldown used for long description

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 This is the official Python library for the VSCP project (https://www.vscp.org).
 
-
 Full documentation is [here](http://www.vscp.org/docs/vscphelper/doku.php?id=start)
 
-----
+
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -23,13 +23,13 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
+    version='0.1.2',
 
     description='Official VSCP Python package',
     long_description=long_description,
+    long_description_content_type="text/markdown",
 
-    # The project's main homepage.
-    url='http://www.vscp.org',
+    url="https://github.com/grodansparadis/pyvscp",
 
     # Author details
     author='Ake Hedman, Grodans Paradis AB',
@@ -68,7 +68,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests' ]),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -79,8 +79,8 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-	'pycrc',
-	'crypto'
+        'pycrc',
+        'crypto'
     ],
 
     # List additional groups of dependencies here (e.g. development
