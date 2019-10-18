1ca881f627035c3efad7ddca88cfdd2d8986d005
Ake Hedman, Grodans Paradis AB <akhe@grodansparadis.com>
2019-10-18 09:15:00 +0200

    Correction in spec by troky

--- vscp/vscp_class.py
+++ vscp/vscp_class.py
@@ -19,7 +19,7 @@
 #            !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
 #                           This file is auto-generated
 #                see https://github.com/grodansparadis/vscp-classes
-#                        Generated: 2019-10-15 11:21:13.387758
+#                        Generated: 2019-10-18 09:04:12.627990
 #
  
 CLASS1_PROTOCOL                          = 0      # VSCP Protocol Functionality
--- vscp/vscp_type.py
+++ vscp/vscp_type.py
@@ -19,7 +19,7 @@
 #            !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
 #                           This file is auto-generated
 #                see https://github.com/grodansparadis/vscp-classes
-#                        Generated: 2019-10-15 11:21:15.326637
+#                        Generated: 2019-10-18 09:04:14.247986
 #
  
  
